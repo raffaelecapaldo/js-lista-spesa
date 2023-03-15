4e742cd46ccd950f00fa4ee37c2f390a56608126
raffaelecapaldo <raffaelecpl@gmail.com>
2023-03-15 14:23:20 +0100

    Added JS

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const shoppingBox = document.getElementById("shopping-list")
+const shoppingList = ["manzo", "salmone", "arachidi", "pollo"]
+
+function scriviLista() {
+    let counter = 0; //contatore
+    while (counter < shoppingList.length) { //finché contatore è minore della quantità dell'array, esegui
+        shoppingBox.innerHTML += `<li class="list-group-item">${shoppingList[[counter]]}</li>`//scrivi li con testo elemento array del ciclo attuale
+        counter++;//incrementa contatore
+    }
+}
+
+scriviLista();
