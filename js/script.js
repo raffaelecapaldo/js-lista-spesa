41747524873e590ab67e1a60bffeacc939d08b72
raffaelecapaldo <raffaelecpl@gmail.com>
2023-03-15 14:24:46 +0100

    Removed useless bracket

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const shoppingList = ["manzo", "salmone", "arachidi", "pollo"]
 function scriviLista() {
     let counter = 0; //contatore
     while (counter < shoppingList.length) { //finché contatore è minore della quantità dell'array, esegui
-        shoppingBox.innerHTML += `<li class="list-group-item">${shoppingList[[counter]]}</li>`//scrivi li con testo elemento array del ciclo attuale
+        shoppingBox.innerHTML += `<li class="list-group-item">${shoppingList[counter]}</li>`//scrivi li con testo elemento array del ciclo attuale
         counter++;//incrementa contatore
     }
 }
