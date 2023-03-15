c60fc035ac8c403a55fcab57f9b173980d5ddf1b
raffaelecapaldo <raffaelecpl@gmail.com>
2023-03-15 15:06:52 +0100

    Now you can remove a product by his n in list

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
 const shoppingBox = document.getElementById("shopping-list")
 const shoppingList = ["pasta", "uova", "gelato al limone", "tiramisù", "peperoni"]
-const addProduct = document.getElementById("add-product");
+const addProduct = document.getElementById("add-product")
+const removeProduct = document.getElementById("remove-product");
+const product = document.getElementById("product");
+const nProduct = document.getElementById("n-product");
+
+
 
 
 function scriviLista() {
     let counter = 0; //contatore
     while (counter < shoppingList.length) { //finché contatore è minore della quantità dell'array, esegui
-        shoppingBox.innerHTML += `<li class="list-group-item">${shoppingList[counter]}</li>`//scrivi li con testo elemento array del ciclo attuale
+        shoppingBox.innerHTML += `<li class="list-group-item">${shoppingList[counter]} </li>`//scrivi li con testo elemento array del ciclo attuale
         counter++;//incrementa contatore
     }
 }
 
+function addAProduct () {
+    shoppingList.push(product.value);//pusha valore input product nell'array
+    shoppingBox.innerHTML = "";//cancella la lista
+    scriviLista();//riscrivila col nuovo array
+}
+
+function removeAProduct () {
+    shoppingList.splice(nProduct.value - 1, 1)// rimuovi un elemento partendo dall'indice scelto dall'utente (la sua lista parte da 1, quindi - 1)
+    shoppingBox.innerHTML = "";//cancella la lista
+    scriviLista();//riscrivila col nuovo array
+
+    
+}
+
 scriviLista();
-const product = document.getElementById("product");
-addProduct.addEventListener("click", function (){
-shoppingList.push(product.value);//pusha valore input product nell'array
-shoppingBox.innerHTML = "";//cancella la lista
-scriviLista();//riscrivila col nuovo array
-})
+addProduct.addEventListener("click", addAProduct);
+removeProduct.addEventListener("click", removeAProduct);
